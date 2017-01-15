c834a2a33a87bcb7885f9941795b5b229b1db350
Farhan Tejani <farhantejani@gmail.com>
2017-01-15 15:21:57 -0500

    Code to generate training and testing data

--- generate_data.py
+++ generate_data.py
@@ -1,25 +1,44 @@
 import numpy as np
-from random import randint
+import pickle
+from random import randint, choice
+import itertools
+import sys
 
 '''
-Determines whether or not there is overlap between 2 rooms.
-This function is used inside of the generate_grids function below.
-Input: room1 = [xmin_1, x_max_1, ymin_1, ymax_1]
-	   room2 = [xmin_2, xmax_2, ymin_2, ymax_2]
+Generates true reward function given a grid and a sentence
+
+Returns vector of size 784
 '''
-def is_overlap(room1, room2):
-	[x_min1, x_max1, y_min1, y_max1] = room1
-	[x_min2, x_max2, y_min2, y_max2] = room2
+def generate_reward(grid, sentence):
+    colors_arr = [20, 30, 40, 50]
+    positive = []
+    for i in xrange(len(colors_arr)):
+        if sentence[i] == 1:
+            positive.append(colors_arr[i])
 
-	intersection = max(0, min(x_max1, x_max2) - max(x_min1, x_min2)) * max(0, min(y_max1, y_max2) - max(y_min1, y_min2))
-	union = (x_max1 - x_min1) * (y_max1 - y_min1) + (x_max2 - x_min2) * (y_max2 - y_min2) - intersection
+    reward = []
+    for num in grid:
+        if num in positive:
+            reward.append(1)
+        else:
+            reward.append(-.01)
+    return reward
 
-	overlap = float(intersection) / float(union)
-    
-	if (overlap > 0):
-		return True
-	else:
-		return False
+'''
+Generates all possible binary vectors of size len
+representing all possible sentences
+
+Returns 2D array
+'''
+def generate_sentences(len=4):
+    strings = [''.join(x) for x in itertools.product('01', repeat=len)]
+    arrays = []
+    for string in strings:
+        vector = []
+        for letter in string:
+            vector.append(int(letter))
+        arrays.append(vector)
+    return arrays
 
 '''
 Generates grids containing 4 rooms, each with a different color.
@@ -31,67 +50,107 @@ Each grid has size 28x28.
 Returns vector of size [num_grids x 784]
 '''
 def generate_grids(num_grids=100, grid_size=28, num_rooms=4):
-	colors = {'red': 20, 'orange': 30, 'yellow': 40, 'green': 50, 'gray': 60}
-	colors_arr = [20, 30, 40, 50]
+    colors = {'red': 20, 'orange': 30, 'yellow': 40, 'green': 50, 'gray': 60}
+    colors_arr = [20, 30, 40, 50]
 
-	grid_count = 0 #Number of grids made so far
-	returned_grids = np.zeros((num_grids, grid_size*grid_size))
+    grid_count = 0  # Number of grids made so far
+    returned_grids = np.zeros((num_grids, grid_size * grid_size))
 
-	#while grid_count < num_grids:
-	#	for height in xrange(1,3):
-	#		for width in xrange(1,3):
+    # Array containing all pairs of allowed height,width combinations for the
+    # rooms.
+    allowed_dimensions = [(height, width)
+                          for height in xrange(1, 3) for width in xrange(1, 3)]
 
-	#Array containing all pairs of allowed height,width combinations for the rooms.
-	allowed_dimensions = [(height, width) for height in xrange(1,3) for width in xrange(1,3)]
+    # Generate all the grids.
+    for i in xrange(num_grids):
+        # Initialize the grid and initialize the color of each square in the
+        # grid to be gray.
+        grid = colors['gray'] * np.ones((grid_size, grid_size))
 
-	#Generate all the grids.
-	for i in xrange(num_grids):
-		#Initialize the grid and initialize the color of each square in the grid to be gray.
-		grid = colors['gray'] * np.ones((grid_size, grid_size))
+        # Randomly select the dimensions of the four rooms.
+        room_dimensions = [allowed_dimensions[
+            randint(0, len(allowed_dimensions) - 1)] for j in xrange(num_rooms)]
 
-		#Randomly select the dimensions of the four rooms.
-		room_dimensions = [allowed_dimensions[randint(0, len(allowed_dimensions) - 1)] for j in xrange(num_rooms)]
+        # Randomly select the locations of the four rooms given their selected
+        # dimensions.
+        room_locations = []
+        for j in xrange(num_rooms):
 
-		#Randomly select the locations of the four rooms given their selected dimensions.
-		room_locations = []
-		for j in xrange(num_rooms):
+            overlap = True
+            x1 = 0
+            y1 = 0
+            height1, width1 = room_dimensions[j]
 
-			overlap = True
-			x1 = 0
-			y1 = 0
-			height1, width1 = room_dimensions[j]
+            # Make sure the newly generated location doesn't overlap with other room locations.
+            # If it does, then pick a new location for the new room.
+            while overlap:
 
-			#Make sure the newly generated location doesn't overlap with other room locations.
-			#If it does, then pick a new location for the new room.
-			while overlap:
+                overlap = False
 
-				overlap = False
+                x1 = randint(0, grid_size - 1 - width)
+                y1 = randint(0, grid_size - 1 - height)
+                room1 = [x1, x1 + width1, y1, y1 + height1]
 
-				x1 = randint(0, grid_size - 1 - width)
-				y1 = randint(0, grid_size - 1 - height)
-				room1 = [x1, x1+width1, y1, y1+height1]
+                if (len(room_locations)) > 0:
+                    for m in xrange(len(room_locations)):
 
-				if (len(room_locations)) > 0:
-					for m in xrange(len(room_locations)):
+                        room2 = room_locations[m]
+                        if is_overlap(room1, room2):
+                            overlap = True
 
-						room2 = room_locations[m]
-						if is_overlap(room1, room2):
-							overlap = True
+            room_locations.append([x1, x1 + width1, y1, y1 + height1])
 
-			room_locations.append([x1, x1+width1, y1, y1+height1])
+            # Color the room the appropriate color in the grid
+            # The color of the room corresponds to which room it is
+            for w in xrange(x1, x1 + width):
+                for h in xrange(y1, y1 + height):
+                    grid[h][w] = colors_arr[j]
 
-			#Color the room the appropriate color in the grid
-			#The color of the room corresponds to which room it is
-			for w in xrange(x1, x1+width):
-				for h in xrange(y1, y1+height):
-					grid[h][w] = colors_arr[j]
+        grid = np.reshape(grid, [1, grid_size * grid_size])
+        returned_grids[i] = grid
 
-		grid = np.reshape(grid, [1, grid_size*grid_size])
-		returned_grids[i] = grid
+    return returned_grids
 
-	return returned_grids
+'''
+Helper function that determines whether or not there is overlap between 2 rooms.
+Input: room1 = [xmin_1, x_max_1, ymin_1, ymax_1]
+	   room2 = [xmin_2, xmax_2, ymin_2, ymax_2]
+'''
+def is_overlap(room1, room2):
+    [x_min1, x_max1, y_min1, y_max1] = room1
+    [x_min2, x_max2, y_min2, y_max2] = room2
+
+    intersection = max(0, min(x_max1, x_max2) - max(x_min1, x_min2)) * \
+        max(0, min(y_max1, y_max2) - max(y_min1, y_min2))
+    union = (x_max1 - x_min1) * (y_max1 - y_min1) + \
+        (x_max2 - x_min2) * (y_max2 - y_min2) - intersection
+
+    overlap = float(intersection) / float(union)
+
+    if (overlap > 0):
+        return True
+    else:
+        return False
 
 
-a = generate_grids()
-print np.reshape(a[0], (28,28))
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 0:
+        print "Usage: python generate_data.py [number] [file_name]"
+    num = int(args[1])
+    file_name = args[2]
+    possible = generate_sentences()
+    sentences = [choice(possible) for _ in range(num)]
+    grids = generate_grids(num)
+    data = []
+    for grid, sentence in zip(grids, sentences):
+        data.append((grid, sentence, generate_reward(grid, sentence)))
+    with open(file_name, 'wb') as fp:
+        pickle.dump(data, fp, protocol=2)
+    #to read:
+    # with open(file_name, 'rb') as fp:
+    #   file = pickle.load(fp)
+    # for grid, sentence, reward in file:
+    #   do stuff
+
 
--- reward_learning.py
+++ reward_learning.py
@@ -1,73 +1,85 @@
-import tensorflow as tf 
+import tensorflow as tf
 sess = tf.InteractiveSession()
 
-#Input is a 28x28 image (flattened to a 1x784 vector)
-#Output is a 28x28 reward map (flattened to a 1x784 vector)
+# Input is a 28x28 image (flattened to a 1x784 vector)
+# Output is a 28x28 reward map (flattened to a 1x784 vector)
 
 state = tf.placeholder(tf.float32, shape=[None, 784])
 sentence_vec = tf.placeholder(tf.float32, shape=[None, 10])
 
-#Maybe change to one-hot vector for each location on the grid.
+# Maybe change to one-hot vector for each location on the grid.
 rewards_actual = tf.placeholder(tf.float32, shape=[None, 784])
 
+
 def weight_variable(shape):
-	initial = tf.truncated_normal(shape, stddev=0.1)
-	return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
 
 def bias_variable(shape):
-	initial = tf.constant(0.1, shape=shape)
-	return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
 
 def conv2d(x, W):
-	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
-	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], 
-		strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
 
-#First convolutional layer
+# First convolutional layer
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
-state_image = tf.reshape(state_image, [-1,28,28,1])
+state_image = tf.reshape(state_image, [-1, 28, 28, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = h_conv1 #max_pool_2x2(h_conv1) #No max pooling for now
+h_pool1 = h_conv1  # max_pool_2x2(h_conv1) #No max pooling for now
 
-#Second convolutional layer
+# Second convolutional layer
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool2, W_conv2) + b_conv2)
-h_pool2 = h_conv2 #max_pool_2x2(h_conv2) #No max pooling for now
+h_pool2 = h_conv2  # max_pool_2x2(h_conv2) #No max pooling for now
 
-#Fully connected layer
+# Fully connected layer
 W_fc1 = weight_variable([7 * 7 * 64, 100])
 b_fc1 = bias_variable([100])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) +  b_fc1)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-#Fully connected layer for the language vector
+# Fully connected layer for the language vector
 W_fc2 = weight_variable([10, 100])
 b_fc2 = bias_variable([100])
 h_fc2 = tf.nn.relu(tf.matmul(sentence_vec, 100) + b_fc2)
 
-#Concatenate fc layers for image and language inputs -> should be a 1x200 vector
-h_fc_concatenated = tf.concat(1, [h_fc1, h_fc2]) #Is 1 the correct dimension to concatenate along?
+# Concatenate fc layers for image and language inputs -> should be a 1x200
+# vector
+# Is 1 the correct dimension to concatenate along?
+h_fc_concatenated = tf.concat(1, [h_fc1, h_fc2])
 
-#Dropout according to input probability
+# Dropout according to input probability
 keep_prob = tf.placeholder(tf.float32)
 h_fc_concatendated = tf.nn.dropout(h_fc_concatenated, keep_prob)
 
-#How to properly do a deconvolution in tensorflow? Are the two lines below correct?
+# How to properly do a deconvolution in tensorflow? Are the two lines
+# below correct?
 W_deconv1 = weight_variable([5, 1, 1, 1])
-h_deconv1 = tf.nn.conv2d_transpose(h_fc_concatenated, filter=W_deconv1, output_shape=[1,28,28,1], strides=[1,1,1,1], padding='SAME')
+h_deconv1 = tf.nn.conv2d_transpose(h_fc_concatenated, filter=W_deconv1, output_shape=[
+                                   1, 28, 28, 1], strides=[1, 1, 1, 1], padding='SAME')
 
-#Should I include another step between the deconvolution and outputting the predicted reward map?
+# Should I include another step between the deconvolution and outputting
+# the predicted reward map?
 
-rewards_pred = tf.reshape(h_deconv1, [-1, 784]) #These are the predicted rewards
+# These are the predicted rewards
+rewards_pred = tf.reshape(h_deconv1, [-1, 784])
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(rewards_pred, rewards_actual))
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(rewards_pred, rewards_actual))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(rewards_pred, 1), tf.argmax(rewards_actual, 1))
+correct_prediction = tf.equal(
+    tf.argmax(rewards_pred, 1), tf.argmax(rewards_actual, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.initialize_all_variables())
